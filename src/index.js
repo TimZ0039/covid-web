40d4819e79b62f9bacdc3828a82cf5dfd76e9a3f
Tim Zhang <mrzhangtim@gmail.com>
2020-08-30 17:01:10 -0700

    covid-web

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
-import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class App extends Component {
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '', testPositiveRate:''};
+  }
+  mySubmitHandler = (event) => {
+    event.preventDefault();
+
+    const url =
+    'https://tim-zhang-covidtracker.herokuapp.com/covid-positive?states='+ this.state.username
+
+   fetch(url)
+     .then((result) => result.json())
+     .then((result) => {
+       this.setState({
+         testPositiveRate: result.testPositiveRate,
+       })
+     })
+  }
+  myChangeHandler = (event) => {
+    this.setState({username: event.target.value});
+  }
   render() {
     return (
-      <div className="App">
-        <h1>Hello World!</h1>
-      </div>
-    )
+      <form onSubmit={this.mySubmitHandler}>
+      <h1>Welcome to Covid Tracker {this.state.username}</h1>
+      <p>Enter the state you want to look at:</p>
+      <input
+        type='text'
+        onChange={this.myChangeHandler}
+      />
+      <input
+        type='submit'
+      />
+      <p>{this.state.testPositiveRate}</p>
+
+      </form>
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<MyForm />, document.getElementById('root'));
